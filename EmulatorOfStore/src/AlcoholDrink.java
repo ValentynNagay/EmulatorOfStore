d4d0f61d62434d07b0face4ca1f963634ae48032
ValentynNagay <nagayvalentyn@gmail.com>
2018-01-29 12:56:32 +0200

    Describing the classes such as AlcoholDrink and SoftDrink

--- EmulatorOfStore/src/AlcoholDrink.java
+++ EmulatorOfStore/src/AlcoholDrink.java
@@ -1,4 +1,25 @@
-
+/**
+ * Describes an alcohol drink
+ * @author Nagay Valentyn
+ *
+ */
 public class AlcoholDrink extends Drink {
+	private String classification;
+	private double strength;
+	
+	/**
+	 * Constructs an object which describes an alcohol drink in a store.
+	 * {@inheritDoc}
+	 * @param classification which a type of alcohol
+	 * @param strength the capacity of alcohol
+	 */
+	public AlcoholDrink(String title, double purchasePrice, double volume, int availability, String classification,
+			double strength) {
+		super(title, purchasePrice, volume, availability);
+		this.classification = classification;
+		this.strength = strength;
+	}
+	
+	
 
 }
--- EmulatorOfStore/src/SoftDrink.java
+++ EmulatorOfStore/src/SoftDrink.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
+/**
+ *  Describes a soft drink
+ * @author Nagay Valentyn
+ *
+ */
 public class SoftDrink extends Drink {
-
+	private String group;
+	private ArrayList<String> composition;
+	
+	/**
+	 * Constructs an object which describes a soft drink in a store.
+	 * {@inheritDoc}
+	 * @param group
+	 * @param composition
+	 */
+	public SoftDrink(String title, double purchasePrice, double volume, int availability, String group,
+			ArrayList<String> composition) {
+		super(title, purchasePrice, volume, availability);
+		this.group = group;
+		this.composition = composition;
+	}
+	
+	
 }
