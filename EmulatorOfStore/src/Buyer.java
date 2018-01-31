a794f112b973ef8afabbbd323839ccf261acacc7
ValentynNagay <nagayvalentyn@gmail.com>
2018-01-31 11:12:04 +0200

    Forming the final version for the class Buyer

--- EmulatorOfStore/src/Buyer.java
+++ EmulatorOfStore/src/Buyer.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 /**
  * Provides minimal functionalities which describe a buyer.
@@ -10,22 +8,28 @@ import java.util.Set;
  */
 public class Buyer {
 	private final int MAX_NUMBER_PRODUCT_FOR_BUYING = 10;
-	
 	private int id;
 	
-
+	/**
+	 * Creates a buyer 
+	 * @param id uses for identification the buyer
+	 */
 	public Buyer(int id) {
 		this.id = id;
 	}
 	
-	
-	
+	/**
+	 * Provides a function for buying drinks from the shop
+	 * @param drinks
+	 * @param shop
+	 * @param day
+	 * @param hour
+	 */
 	public void buyRandomProducts(ArrayList<Drink> drinks, Shop shop, int day, int hour) {
-		System.out.println("Buyer with id = " + id + " has bought those drinks:");
 		Random rand = new Random();
 		int quantityOfProducts = rand.nextInt(MAX_NUMBER_PRODUCT_FOR_BUYING) + 1;
 		
-		
+		System.out.println("Buyer with id = " + id + " has bought those drinks:");
 		int i = 0;
 		while(i < quantityOfProducts) {
 			
@@ -39,15 +43,15 @@ public class Buyer {
 				}
 			}
 			
+			// if the shop isn't empty
 			if(!isEmpty) {
-				Drink soldDrink = drinks.get(rand.nextInt(drinks.size()));
-				
-				//quantityOfOneProduct = quantityOfProducts - quantityOfOneProduct;
-				int quantityOfOneProduct = rand.nextInt(quantityOfProducts) + 1;
+				Drink soldDrink = drinks.get(rand.nextInt(drinks.size())); // gets the random drink for selling
+				int quantityOfOneProduct = rand.nextInt(quantityOfProducts) + 1; // sets the random quantity of the drinks
 				
 				if ((i + quantityOfOneProduct) <= quantityOfProducts) {
 					if (soldDrink.getQuantity() >= quantityOfOneProduct) {
 						
+						// chooses the politic of the mark up for selling drinks
 						if (quantityOfOneProduct > 2) {
 							shop.applyGrossMarkUp(soldDrink, quantityOfOneProduct);
 						} else if (hour >= 18 && hour <= 20) {
@@ -65,12 +69,10 @@ public class Buyer {
 				} else {
 					continue;
 				}
-				
 			} else {
 				System.out.println("The shop is empty today. Come here tomorrow, please.");
 				break;
-			}
-			
+			}	
 		}
 	}
 	
